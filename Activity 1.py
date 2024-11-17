83e57397e2313c77db14a9f310ff5a11f40881e8
Avanthikacoding <aviedayilliam@gmail.com>
2024-11-17 12:29:44 +0000

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,14 @@
+print("Hello and welcome to lesson 2!")
+print (25)
+print ("My age is", 12)
+print("I love python", end= '!!!')
+print('\n')
+ #Activity 2 
+a = 50
+x = 25
+Avanthika = 'Avi'
+print(a)
+print(x)
+print(Avanthika)
+name = input("What is your name?")
+print(name)
